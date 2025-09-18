eaaf4a62dace01feaa55298cefc10ad94cbdf2f8
jji3379 <jji3379@naver.com>
2025-09-18 20:39:57 +0900

    feat: 모듈 분리로 MSA 전환

---
+++ point/src/main/java/com/example/point/intrastructure/PointRepository.java
@@ -0,0 +1,12 @@
+package com.example.point.intrastructure;
+
+import com.example.point.domain.Point;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PointRepository extends JpaRepository<Point, Long> {
+    Optional<Point> findByUserId(Long userId);
+}
